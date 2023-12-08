48325226dac725aa732497f31e9ebe9624fd658e
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2023-12-08 10:38:12 +0900

    make用ファイル

---
+++ PROMELA2CS/gen.sh
@@ -0,0 +1,12 @@
+PARSER=/Users/yamakawamanobe/Master/promela_compiler/pmlparser2.py
+EDIT=/Users/yamakawamanobe/Master/promela_compiler/edit_result_analyze.py
+DIR=/Users/yamakawamanobe/Master/promela_compiler
+
+python3 $PARSER y $DIR/$1 | python3 $EDIT > hoge.txt
+echo "convartTree:start2(" `cat hoge.txt` ")."\
+ |erl
+cat result_tree.txt\
+ | perl -e 'while (<>) { chop; print " $_"; } printf("\n");' > hoge2.txt
+# echo "pml2cs:start(" `cat hoge2.txt` ")."\
+#  |erl
+#  | perl -e 'while (<>) { chop; print " $_"; }' > hoge2.txt
