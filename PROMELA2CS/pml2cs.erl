167471c2eb59cec59e6b1a1594639de963b820f2
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2023-12-14 13:07:26 +0900

    do対応

--- PROMELA2CS/pml2cs.erl
+++ PROMELA2CS/pml2cs.erl
@@ -1,147 +1,200 @@
 -module(pml2cs).
--export([start/1, module/2, sequence/3]).
+-export([start/1, sequence/3]).
+-import(lists, [map/2]).
 -include("record.hrl").
 
 start(Tree) ->
-    spec(Tree, 0).
+% start() ->
+%     {ok, IoDevice} = file:open("hoge2.txt", read),
+%     {ok, Data} = file:read_line(IoDevice),
+    % io:format("Data: ~p~n", [Data]),
 
-spec(Tree, Pid) when is_tuple(Tree#tree.children) ->
+    spec(Tree).
+
+spec(Tree) ->
     Module = Tree#tree.children,
-    module(Module, Pid);
-    % io:format("tuple: ~p~n ", [Module]);
-spec(Tree, Pid) when is_list(Tree#tree.children) ->
-    Modulelist = Tree#tree.children,
-    my_utility:listloop(Modulelist, Pid);
-    % io:format("list: ~p~n ", [Module]);
-spec(Tree, Pid) ->
-    io:format("spec is nether a list nor a tule: ~p~n ", [Tree]).
+    module(Module).
 
-module(Module, Pid) ->
+module([]) ->                       %Moduleのリストの各要素を調べる
+    fin;
+module([Module|Modules]) ->
     Child = Module#tree.children,
-    ChildValue = Child#tree.value,
-    case ChildValue of
-        NT when NT =:= proctype orelse NT =:= init ->
-            NewPid = spawn(fun setmanager:start/0),
-            genPG(Child, NewPid);
-        NT when NT =:= utype orelse NT =:= mtype orelse NT =:= decl_lst ->
+    case Child of
+        [_|_] ->
             io:format("hensuu sengen desu~n");
+        %プロセスならPG生成の関数へ
+        Proc when Proc#tree.value =:= proctype orelse Proc#tree.value =:= init ->
+            Pid = spawn(fun setmanager:start/0),
+            genPG(Child, Pid);
+        Decl when Decl#tree.value =:= utype orelse Decl#tree.value =:= mtype ->
+            io:format("declaration desu~n");
         _ ->
-            io:format("module is nether a process nor declaration: ~p~n", [ChildValue])
-    end.
+            io:format("module is neither a process nor declaration: ~p~n", [Child#tree.value])
+    end,
+    module(Modules).
 
 genPG(Proc, Pid) ->
     ProcChild = Proc#tree.children,
-    my_utility:listloop4seq(ProcChild, Pid, 0).
+    Fin = my_utility:listloop(ProcChild, Pid, 0),       %proctype/initの子供のリストから[step]を見つけ出してpml2csに返す関数を呼びだし
+    Pid ! {self(), Fin}.
 
-sequence(Sequence, Pid, I) when is_tuple(Sequence#tree.children) ->
-    Step = Sequence#tree.children,
-    step(Step, Pid, I);
-sequence(Sequence, Pid, I) when is_list(Sequence#tree.children) ->
-    Steps = Sequence#tree.children,
-    steps(Steps, Pid, I).
+% sequence([], Pid, I) ->
+%     ok;
 
+%stepのリストを受け取り各stepの戻り先とActを求める関数へ要素を一つづつ渡す
+sequence([Step|[]], Pid, I) ->
+    my_utility:addloc(I, Pid),
+    {StepAtom, Result} = step(Step, Pid, I),
+        case StepAtom of
+        stmnt2 ->
+            {NewI, Flag} = Result,
+            case Flag of
+                0 ->
+                    fin;
+                1 ->
+                    my_utility:genedge(NewI, {true, skip}, exit, Pid)
+            end;
+        stmnt81314 ->
+            Act = Result,
+            my_utility:addact(Act, Pid),
+            my_utility:genedge(I, Act, exit, Pid);
+        one_decl ->
+            Act = Result,
+            my_utility:addact(Act, Pid),
+            my_utility:genedge(I, Act, exit, Pid)
+    end;
+    % my_utility:addact(Act, Pid),
+    % my_utility:genedge(I, Act, exit, Pid);
+sequence([Step|Steps], Pid, I) ->
+    my_utility:addloc(I, Pid),
+    {StepAtom, Result} = step(Step, Pid, I),
+    case StepAtom of
+        stmnt2 ->
+            {NewI, Flag} = Result,
+            case Flag of
+                0 ->
+                    fin;
+                1 ->
+                    sequence(Steps, Pid, NewI)
+            end;
+        stmnt81314 ->
+            Act = Result,
+            my_utility:addact(Act, Pid),
+            NewI = I + 1,
+            my_utility:genedge(I, Act, NewI, Pid),
+            sequence(Steps, Pid, NewI);
+        one_decl ->
+            Act = Result,
+            my_utility:addact(Act, Pid),
+            NewI = I + 1,
+            my_utility:genedge(I, Act, NewI, Pid),
+            sequence(Steps, Pid, NewI)
+    end.
+
+%stepを解析する関数,step1ならstmntを解析する関数へ，step2ならone_declを解析する関数へ
+% tuple(step tree) -> tuple({stepがなにか, Act})
 step(Step, Pid, I) ->
+    case Step#tree.value of
+        step1 ->
+            [Stmnt|StmntOpt] = Step#tree.children,
+            {StmntAtom, Result} = stmnt(Stmnt, Pid, I),
+            {StmntAtom, Result};
+        step2 ->
+            Result = one_decl(Step#tree.children, Pid, I),
+            Result
+    end.
+
+one_decl([One_decl], Pid, I) ->
+    io:format("one_decl~n"),
+    Act = One_decl#tree.children,
+    {one_decl, Act}.
+
+%tuple(tree record) -> tuple({atom, {Act, Arrow}})
+%stmntを解析する関数，stmntの種類によって場合分け
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt1->      %stmnt1: if
+    io:format("stmnt1~n");
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt2 ->     %stmnt2: do
+    io:format("stmnt2~n"),
+    Result = options(Stmnt#tree.children,I, [], Pid, I, 0),    %Result -> {NewI, BreakFlag}
+    {stmnt2, Result};
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt4 ->     %stmnt4: atomic
+    io:format("stmnt4~n");
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt9 ->     %stmnt9: else
+    io:format("stmnt9~n");
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt10 ->    %stmnt10: break
+    io:format("stmnt10~n"),
+    Act = Stmnt,
+    {stmnt10, Act};
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt8 orelse Stmnt#tree.value == stmnt13 orelse Stmnt#tree.value == stmnt14 ->       %stmnt8: send receive assign expr, stmnt13: printm stmnt14: printf
+    io:format("stmnt8or13or14:~p~n", [Stmnt]),
+    Act = Stmnt,
+    {stmnt81314, Act}.
+
+stmnt4guard(Stmnt4guard, Pid, I) when Stmnt4guard#tree.value == stmnt4guard1->
+    Stmnt4guard#tree.children;
+stmnt4guard(Stmnt4guard, Pid, I) when Stmnt4guard#tree.value == stmnt4guard2->
+    Stmnt4guard#tree.children.
+
+% list([optionseq]) -> tuple({optionの中で進めた分だけインクリメントしたI, 最後のoptionseqの最後のstepのAct})
+% optionseqの下の[step]の最後のstepをスタックにつんで最後にスタックの中身のエッジをはる(Targetはdoの繰り返し先)
+options([], Source, Stack, Pid, I, Flag) ->
+    NewI = my_utility:genedgeStack(Stack, Source, Pid, I, 0),
+    % io:format("NewI;~p~n", [NewI]),
+    {NewI, Flag};
+options([Optionseq|Optionseqs], Source, Stack, Pid, I, Flag) ->               %breakの場合はスタックに積まずに再帰
+    {NewI, Act, BreakFlag} = optionseq(Optionseq, Source, Pid, I),
+    NewStack = my_utility:push(Stack, {NewI, Act, BreakFlag}),
+    options(Optionseqs, Source, NewStack, Pid, NewI, BreakFlag).
+
+
+% optionseq   : guard [step]
+% [step]の最後の要素のstepのActを返す
+optionseq(Optionseq, Source, Pid, I) ->
+    [GuardTree|StepList] = Optionseq#tree.children,
+    Guard = guard(GuardTree, Pid, I),
+    {NewI, Act, BreakFlag} = sequence4opt(hd(StepList), Source, Guard, Pid, I),
+    {NewI, Act, BreakFlag}.
+
+%optionseqの[step]を解析．[step]の最後までのエッジをはる
+sequence4opt([Step|[]], Source, Guard, Pid, I) ->
     my_utility:addloc(I, Pid),
-    StepChild = Step#tree.children,
-    StepChildValue = StepChild#tree.value,
-    case StepChildValue of
-        stmnt ->
-            stmnt(Step#tree.children, Step, Pid, I);
+    {StepAtom, Result} = step(Step, Pid, I),
+    case StepAtom of
+        stmnt10 ->          %break
+            Act = Result,
+            {I, {Guard,Act}, 1};
+        stmnt81314 ->
+            Act = Result,
+            {I, {Guard,Act}, 0};
         one_decl ->
-            Act = {true, StepChild#tree.children},
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, exit, Pid),
-            Pid ! {self(), fin}
-    end.
-
-steps([Step|Steps], Pid, I) ->
+            Act = Result,
+            {I, {Guard, Act}, 0}
+    end;
+    % {I, Act, 0};       %最後のstepのラベル番号とそのAct
+sequence4opt([Step|Steps], Source, Guard, Pid, I) ->
     my_utility:addloc(I, Pid),
-    StepChild = Step#tree.children,
-    StepChildValue = StepChild#tree.value,
-    case StepChildValue of
-        stmnt ->
-            stepsstmnt(StepChild, Steps, Pid, I);
+    {StepAtom, Result} = step(Step, Pid, I),
+    case StepAtom of
+        % stmnt2 ->                   %do
+        %     fin;
+        stmnt10 ->                  %break
+            Act = Result,
+            {I, Act, 1};
+        stmnt81314 ->
+            Act = Result,
+            my_utility:addact(Act, Pid),
+            NewI = I + 1,
+            my_utility:genedge(Source, {Guard, Act}, NewI, Pid),
+            sequence4opt(Steps, NewI, true, Pid, NewI);
         one_decl ->
-            Act = {true,StepChild#tree.children},
+            Act = Result,
             my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, I + 1, Pid),
             NewI = I + 1,
-            Tmp = hd(Steps),
-            steps2(Tmp, Pid, NewI)
+            my_utility:genedge(Source, {Guard, Act}, NewI, Pid),
+            sequence4opt(Steps, NewI, true, Pid, NewI)
     end.
 
-steps2(Steps, Pid, I) ->
-    my_utility:addloc(I, Pid),
-    StepsChild = Steps#tree.children,
-    [_ | Sequence] = StepsChild,
-    io:format("sequence:~p~n", [Sequence]),
-    case length(Sequence) of
-        1 ->
-            step(hd(Sequence), Pid, I);
-        _ ->
-            steps(Sequence, Pid, I)
-    end.
-    % steps(Sequence, Pid, I).
-
-    % StepChildValue = StepChild#tree.value,
-    % case StepChildValue of
-    %     stmnt ->
-    %         io:format("stepsstmnt:~p~n", [StepChild]),
-    %         Pid!{self(), fin};
-    %     one_decl ->
-    %         Act = StepChild#tree.children,
-    %         my_utility:addact(Act, Pid),
-    %         case Steps of
-    %             [] ->
-    %                 my_utility:genedge(I, Act, exit, Pid),
-    %                 NewI = I,
-    %                 Pid!{self(), fin};
-    %             _ ->
-    %                 my_utility:genedge(I, Act, I + 1, Pid),
-    %                 NewI = I + 2,
-    %                 steps2(Steps, Pid, I)
-    %         end
-    % end.
-
-
-stmnt(Stmnt, Step, Pid, I) when is_tuple(Stmnt#tree.children) ->
-    StmntChild = Stmnt#tree.children,
-    StmntChildValue = StmntChild#tree.value,
-    case StmntChildValue of
-        X when X =:= else ->
-            my_utility:addact({true,"else"}, Pid),
-            my_utility:genedge(I, "true:else", exit, Pid),
-            NewI = I + 1,
-            Pid ! {self(), fin};
-        X when X =:= assign ->
-            Act = {true,StmntChild#tree.children},
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, exit, Pid),
-            NewI = I + 1,
-            Pid ! {self(), fin}
-    end.
-
-% stmnt(Stmnt Step, Pid, I) when is_list(Stmnt#tree.children) ->
-%     GetchildValue = fun(Child) -> Child#tree.value end,
-
-%     StmntChildValue = GetchildValue(Stmnt#tree.children),
-%     case StmntChildValue of
-%         % if or do
-
-stepsstmnt(Stmnt, Steps, Pid, I) when is_tuple(Stmnt#tree.children) ->
-    StmntChild = Stmnt#tree.children,
-    StmntChildValue = StmntChild#tree.value,
-    case StmntChildValue of
-        X when X =:= else ->
-            Act = {true, "else"},
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, I + 1, Pid),
-            NewI = I + 2,
-            steps2(Steps, Pid, NewI);
-        X when X =:= assign ->
-            Act = {true, StmntChild#tree.children},
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, I + 1, Pid),
-            NewI = I + 2,
-            steps2(Steps, Pid, NewI)
-    end.
+guard(GuardTree, Pid, I) ->
+    [Stmnt4guard|GuardOpt] = GuardTree#tree.children,
+    Guard = stmnt4guard(Stmnt4guard, Pid, I),
+    Guard.
