b5493e1c87635bdff233d0719197e719aa8d14fd
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2023-12-14 13:28:41 +0900

    do対応

--- PROMELA2CS/my_utility.erl
+++ PROMELA2CS/my_utility.erl
@@ -1,39 +1,60 @@
 -module(my_utility).
--export([listloop/2, listloop4seq/3, genedge/4, addloc/2, addact/2]).
+-export([push/2, pop/1, genedgeStack/5,listloop/3, genedge/4, addloc/2, addact/2]).
 -include("record.hrl").
 
+push(Stack, Value) ->
+    [Value] ++ Stack.
 
-listloop([], Pid) ->
-    ok;
-listloop([Tuple|T], Pid) ->
-    Value = Tuple#tree.value,
-    case Value of
-        module  ->
-            io:format("aa:~p~n", [Value]),
-            pml2cs:module(Tuple, Pid),
-            listloop(T, Pid);
-        % sequence ->
-        %     io:format("bb:~p~n", [Value]),
-        %     pml2cs:sequence(Tuple, Pid),
-        %     listloop(T, Pid);
-        _       ->
-            io:format("cc:~p~n", [Value]),
-            listloop(T, Pid)
+pop(Stack) ->
+    case Stack of
+        [H|T] ->
+            {H, T}
     end.
 
-listloop4seq([], Pid, I) ->
-    ok;
-listloop4seq([Tuple|T], Pid, I) ->
-    Value = Tuple#tree.value,
-    case Value of
-        sequence ->
-            io:format("bb:~p~n", [Value]),
-            pml2cs:sequence(Tuple, Pid, I),
-            listloop4seq(T, Pid, I);
-        _        ->
-            io:format("cc:~p~n", [Value]),
-            listloop4seq(T, Pid, I)
+% 第5引数の0/1はBreakFlagが１のやつがスタックの中にあったかどうかで切り替わる
+% あったばあいはbreakしたあとのIを返す
+genedgeStack([], _, _, I, 0) ->
+    I;
+genedgeStack([], _, _, I, 1) ->
+    I + 1;
+genedgeStack(Stack, Target, Pid, I, 0) ->
+    {{Source, Act, BreakFlag},T} = pop(Stack),
+    addact(Act, Pid),
+    case BreakFlag of
+        0 ->
+            genedge(Source, Act, Target, Pid),
+            genedgeStack(T, Target, Pid, I, 0);
+        1 ->
+            genedge(Source, Act, I+1, Pid),
+            genedgeStack(T, Target, Pid, I, 1)
+    end;
+genedgeStack(Stack, Target, Pid, I, 1) ->
+    {{Source, Act, BreakFlag},T} = pop(Stack),
+    addact(Act, Pid),
+    case BreakFlag of
+        0 ->
+            genedge(Source, Act, Target, Pid),
+            genedgeStack(T, Target, Pid, I, 1);
+        1 ->
+            genedge(Source, Act, I+1, Pid),
+            genedgeStack(T, Target, Pid, I, 1)
     end.
+    % case Stack of
+    %     [_|[]] ->
+    %         fin;
+    %     [_|T] ->
+    %         genedgeStack(T, Target, Pid)
+    % end.
+
+%proctypeの子供のリストから[step]を見つけ出してpml2csに返す
+listloop([], _, _) ->
+    fin;
+listloop([H|[]], Pid, I) ->
+    pml2cs:sequence(H, Pid, I),
+    listloop([], Pid, I);
+listloop([H|T], Pid, I) ->
+    io:format("H:~p~n", [H]),
+    listloop(T, Pid, I).
 
 genedge(Source, Act, Target, Pid) ->
     Pid ! {self(), {arrow, {Source, Act, Target}}},
