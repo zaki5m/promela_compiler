fc8cbb5ac2104352f4f50dfaa51a8924f0fd3ee4
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2023-12-16 13:20:03 +0900

    ネストしたdoに対応

--- PROMELA2CS/my_utility.erl
+++ PROMELA2CS/my_utility.erl
@@ -1,5 +1,5 @@
 -module(my_utility).
--export([push/2, pop/1, genedgeStack/6,listloop/3, genedge/4, addloc/2, addact/2]).
+-export([push/2, pop/1, genedgeStack/4,listloop/4, genedge/4, addloc/2, addact/2]).
 -include("record.hrl").
 
 push(Stack, Value) ->
@@ -11,64 +11,29 @@ pop(Stack) ->
             {H, T}
     end.
 
-% 第5引数の0/1はBreakFlagが１のやつがスタックの中にあったかどうかで切り替わる
-% あったばあいはbreakしたあとのIを返す
-genedgeStack([], _, _, I, 0, _) ->
-    I;
-genedgeStack([], _, _, I, 1, 0) ->
+genedgeStack([], _, _, I) ->
     I + 1;
-genedgeStack([], _, _, I, 1, 1) ->
-    I;
-genedgeStack(Stack, Target, Pid, I, 0, SeqFlag) ->
-    {{Source, Act, BreakFlag},T} = pop(Stack),
-    addact(Act, Pid),
+genedgeStack(Stack, Target, Pid, I) ->
+    {{Source, BreakFlag}, T} = pop(Stack),
+    io:format("NewI:~pFlag:~p~n", [Source, BreakFlag]),
     case BreakFlag of
         0 ->
-            genedge(Source, Act, Target, Pid),
-            genedgeStack(T, Target, Pid, I, 0, SeqFlag);
+            genedge(Source, {true,skip}, Target, Pid),
+            genedgeStack(T, Target, Pid, I);
         1 ->
-            case SeqFlag of
-                0 ->
-                    genedge(Source, Act, I+1, Pid),
-                    genedgeStack(T, Target, Pid, I, 1, SeqFlag);
-                1 ->
-                    genedge(Source, Act, Target, Pid),
-                    genedgeStack(T, Target, Pid, I, 1, SeqFlag)
-            end
-    end;
-genedgeStack(Stack, Target, Pid, I, 1, SeqFlag) ->
-    {{Source, Act, BreakFlag},T} = pop(Stack),
-    addact(Act, Pid),
-    case BreakFlag of
-        0 ->
-            genedge(Source, Act, Target, Pid),
-            genedgeStack(T, Target, Pid, I, 1, SeqFlag);
-        1 ->
-            case SeqFlag of
-                0 ->
-                    genedge(Source, Act, I+1, Pid),
-                    genedgeStack(T, Target, Pid, I, 1, SeqFlag);
-                1 ->
-                    genedge(Source, Act, Target, Pid),
-                    genedgeStack(T, Target, Pid, I, 1, SeqFlag)
-            end
+            genedge(Source, {true,skip}, I+1, Pid),
+            genedgeStack(T, Target, Pid, I)
     end.
-    % case Stack of
-    %     [_|[]] ->
-    %         fin;
-    %     [_|T] ->
-    %         genedgeStack(T, Target, Pid)
-    % end.
 
 %proctypeの子供のリストから[step]を見つけ出してpml2csに返す
-listloop([], _, _) ->
-    fin;
-listloop([H|[]], Pid, I) ->
-    pml2cs:sequence(H, Pid, I),
-    listloop([], Pid, I);
-listloop([H|T], Pid, I) ->
+listloop([], _, I, Fin) ->
+    {Fin, I};
+listloop([H|[]], Pid, I, Fin) ->
+    {NewFin, NewI, _} = pml2cs:sequence(H, I, true, Pid, I),
+    listloop([], Pid, NewI, NewFin);
+listloop([H|T], Pid, I, Fin) ->
     io:format("H:~p~n", [H]),
-    listloop(T, Pid, I).
+    listloop(T, Pid, I, Fin).
 
 genedge(Source, Act, Target, Pid) ->
     Pid ! {self(), {arrow, {Source, Act, Target}}},
--- PROMELA2CS/pml2cs.erl
+++ PROMELA2CS/pml2cs.erl
@@ -1,14 +1,9 @@
 -module(pml2cs).
--export([start/1, sequence/3]).
+-export([start/1, sequence/5]).
 -import(lists, [map/2]).
 -include("record.hrl").
 
 start(Tree) ->
-% start() ->
-%     {ok, IoDevice} = file:open("hoge2.txt", read),
-%     {ok, Data} = file:read_line(IoDevice),
-    % io:format("Data: ~p~n", [Data]),
-
     spec(Tree).
 
 spec(Tree) ->
@@ -35,71 +30,60 @@ module([Module|Modules]) ->
 
 genPG(Proc, Pid) ->
     ProcChild = Proc#tree.children,
-    Fin = my_utility:listloop(ProcChild, Pid, 0),       %proctype/initの子供のリストから[step]を見つけ出してpml2csに返す関数を呼びだし
-    Pid ! {self(), Fin}.
-
-% sequence([], Pid, I) ->
-%     ok;
+    {Fin, NewI} = my_utility:listloop(ProcChild, Pid, 0, fin),       %proctype/initの子供のリストから[step]を見つけ出してpml2csに返す関数を呼びだし
+    case Fin of
+        fin ->
+            my_utility:genedge(NewI, {true,skip}, exit, Pid);
+        notfin ->
+            pass
+    end,
+    Pid ! {self(), fin}.
 
 %stepのリストを受け取り各stepの戻り先とActを求める関数へ要素を一つづつ渡す
-sequence([Step|[]], Pid, I) ->
+sequence([], Source, Guard, Pid, I) ->
+    {fin, I, 0};
+sequence([Step|Steps], Source, Guard, Pid, I) ->
     my_utility:addloc(I, Pid),
-    {StepAtom, Result} = step(Step, Pid, I, 0),
-        case StepAtom of
-        stmnt2 ->
-            {NewI, Flag} = Result,
-            case Flag of
-                0 ->
-                    fin;
-                1 ->
-                    my_utility:genedge(NewI, {true, skip}, exit, Pid)
-            end;
-        stmnt81314 ->
-            Act = Result,
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, exit, Pid);
-        one_decl ->
-            Act = Result,
-            my_utility:addact(Act, Pid),
-            my_utility:genedge(I, Act, exit, Pid)
-    end;
-    % my_utility:addact(Act, Pid),
-    % my_utility:genedge(I, Act, exit, Pid);
-sequence([Step|Steps], Pid, I) ->
-    my_utility:addloc(I, Pid),
-    {StepAtom, Result} = step(Step, Pid, I, 0),
+    {StepAtom, Result} = step(Step, Pid, I),
     case StepAtom of
-        stmnt2 ->
+        stmnt2 ->       %do
             {NewI, Flag} = Result,
             %doの中にbreakがない（Flagが０）の場合は無限ループするのでこれ以降のstepに枝をはらない
             case Flag of
                 0 ->
-                    fin;
+                    {notfin, NewI, 0};
                 1 ->
-                    sequence(Steps, Pid, NewI)
+                    % my_utility:genedge(I, {true,skip}, NewI, Pid),
+                    sequence(Steps, NewI, true, Pid, NewI)
             end;
+        stmnt10 ->                  %break
+            Act = Result,
+            my_utility:addact({Guard,Act}, Pid),
+            NewI = I + 1,
+            my_utility:genedge(Source, {Guard,Act}, NewI, Pid),
+            {fin, NewI, 1};
         stmnt81314 ->
             Act = Result,
-            my_utility:addact(Act, Pid),
+            my_utility:addact({Guard,Act}, Pid),
             NewI = I + 1,
-            my_utility:genedge(I, Act, NewI, Pid),
-            sequence(Steps, Pid, NewI);
+            my_utility:genedge(Source, {Guard,Act}, NewI, Pid),
+            sequence(Steps, NewI, true, Pid, NewI);
         one_decl ->
             Act = Result,
-            my_utility:addact(Act, Pid),
+            my_utility:addact({Guard,Act}, Pid),
             NewI = I + 1,
-            my_utility:genedge(I, Act, NewI, Pid),
-            sequence(Steps, Pid, NewI)
+            my_utility:genedge(Source, {Guard,Act}, NewI, Pid),
+            sequence(Steps, NewI, true, Pid, NewI)
     end.
 
 %stepを解析する関数,step1ならstmntを解析する関数へ，step2ならone_declを解析する関数へ
 % tuple(step tree) -> tuple({stepがなにか, Act})
 % SeqFlagは０→sequenceから呼び出された，１→sequence4optから呼び出された
-step(Step, Pid, I, SeqFlag) ->
+step(Step, Pid, I) ->
     case Step#tree.value of
         step1 ->
             [Stmnt|StmntOpt] = Step#tree.children,
-            {StmntAtom, Result} = stmnt(Stmnt, Pid, I, SeqFlag),
+            {StmntAtom, Result} = stmnt(Stmnt, Pid, I),
             {StmntAtom, Result};
         step2 ->
             Result = one_decl(Step#tree.children, Pid, I),
@@ -113,22 +97,21 @@ one_decl([One_decl], Pid, I) ->
 
 %tuple(tree record) -> tuple({atom, {Act, Arrow}})
 %stmntを解析する関数，stmntの種類によって場合分け
-stmnt(Stmnt, Pid, I, _) when Stmnt#tree.value == stmnt1->      %stmnt1: if
+stmnt(Stmnt, Pid, I_) when Stmnt#tree.value == stmnt1->      %stmnt1: if
     io:format("stmnt1~n");
-    % Result = 
-stmnt(Stmnt, Pid, I, SeqFlag) when Stmnt#tree.value == stmnt2 ->     %stmnt2: do
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt2 ->     %stmnt2: do
     io:format("stmnt2~n"),
-    Result = options(Stmnt#tree.children,I, [], Pid, I, 0, SeqFlag),    %Result -> {NewI, BreakFlag}
+    Result = options(Stmnt#tree.children,I, [], Pid, I, 0),    %Result -> {NewI, BreakFlag}
     {stmnt2, Result};
-stmnt(Stmnt, Pid, I, _) when Stmnt#tree.value == stmnt4 ->     %stmnt4: atomic
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt4 ->     %stmnt4: atomic
     io:format("stmnt4~n");
-stmnt(Stmnt, Pid, I, _) when Stmnt#tree.value == stmnt9 ->     %stmnt9: else
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt9 ->     %stmnt9: else
     io:format("stmnt9~n");
-stmnt(Stmnt, Pid, I, _) when Stmnt#tree.value == stmnt10 ->    %stmnt10: break
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt10 ->    %stmnt10: break
     io:format("stmnt10~n"),
     Act = Stmnt,
     {stmnt10, Act};
-stmnt(Stmnt, Pid, I, _) when Stmnt#tree.value == stmnt8 orelse Stmnt#tree.value == stmnt13 orelse Stmnt#tree.value == stmnt14 ->       %stmnt8: send receive assign expr, stmnt13: printm stmnt14: printf
+stmnt(Stmnt, Pid, I) when Stmnt#tree.value == stmnt8 orelse Stmnt#tree.value == stmnt13 orelse Stmnt#tree.value == stmnt14 ->       %stmnt8: send receive assign expr, stmnt13: printm stmnt14: printf
     io:format("stmnt8or13or14:~p~n", [Stmnt]),
     Act = Stmnt,
     {stmnt81314, Act}.
@@ -141,89 +124,27 @@ stmnt4guard(Stmnt4guard, Pid, I) when Stmnt4guard#tree.value == stmnt4guard2->
 % list([optionseq]) -> tuple({optionの中で進めた分だけインクリメントしたI, Flag})
 % optionseqの下の[step]の最後のstepをスタックにつんで最後にスタックの中身のエッジをはる(Targetはdoの繰り返し先)
 % 第五引数のFlagはbreakを含んでいるかどうか0→含まない，1→含む
-options([], Source, Stack, Pid, I, Flag, SeqFlag) ->
-    NewI = my_utility:genedgeStack(Stack, Source, Pid, I, 0, SeqFlag),
-    % io:format("NewI;~p~n", [NewI]),
+options([], Source, Stack, Pid, I, Flag) ->
+    NewI = my_utility:genedgeStack(Stack, Source, Pid, I),
     {NewI, Flag};
-options([Optionseq|Optionseqs], Source, Stack, Pid, I, Flag, SeqFlag) ->               %breakの場合はスタックに積まずに再帰
+options([Optionseq|Optionseqs], Source, Stack, Pid, I, Flag) ->               %breakの場合はスタックに積まずに再帰
     Result = optionseq(Optionseq, Source, Pid, I),
-    case Result of
-        {fin, NewI, BreakFlag} ->
-            options(Optionseqs, Source, Stack, Pid, NewI, BreakFlag, SeqFlag);
-        _ ->
-            {NewI, Act, BreakFlag} = Result,
-            NewStack = my_utility:push(Stack, {NewI, Act, BreakFlag}),
-            options(Optionseqs, Source, NewStack, Pid, NewI, BreakFlag, SeqFlag)
+    {_, NewI, BreakFlag} = Result,
+    NewStack = my_utility:push(Stack, {NewI, BreakFlag}),
+    case Flag of
+        0 ->
+            options(Optionseqs, Source, NewStack, Pid, NewI, BreakFlag);
+        1 ->
+            options(Optionseqs, Source, NewStack, Pid, NewI, Flag)
     end.
-
 
 % optionseq   : guard [step]
 % [step]の最後の要素のstepのActを返す
 optionseq(Optionseq, Source, Pid, I) ->
     [GuardTree|StepList] = Optionseq#tree.children,
     Guard = guard(GuardTree, Pid, I),
-    Result= sequence4opt(hd(StepList), Source, Guard, Pid, I),
+    Result = sequence(hd(StepList), Source, Guard, Pid, I),     %Result->{fin, NewI, BreakFlag}
     Result.
-    % case Result of
-    %     {fin, NewI, Flag} ->
-    %         {fin, NewI, Flag};
-    %     _ ->
-    %         {NewI, Act, BreakFlag} = Result,
-    %         {NewI, Act, BreakFlag}
-    % end.
-
-%optionseqの[step]を解析．[step]の最後までのエッジをはる
-sequence4opt([Step|[]], Source, Guard, Pid, I) ->
-    my_utility:addloc(I, Pid),
-    {StepAtom, Result} = step(Step, Pid, I, 1),
-    case StepAtom of
-        stmnt2 ->
-            {NewI, Flag} = Result,
-            case Flag of
-                0 ->
-                    {fin, NewI, Flag};
-                1 ->
-                    {NewI, {Guard, skip}, 1}
-            end;
-        stmnt10 ->          %break
-            Act = Result,
-            {I, {Guard,Act}, 1};
-        stmnt81314 ->
-            Act = Result,
-            {I, {Guard,Act}, 0};
-        one_decl ->
-            Act = Result,
-            {I, {Guard, Act}, 0}
-    end;
-    % {I, Act, 0};       %最後のstepのラベル番号とそのAct
-sequence4opt([Step|Steps], Source, Guard, Pid, I) ->
-    my_utility:addloc(I, Pid),
-    {StepAtom, Result} = step(Step, Pid, I, 1),
-    case StepAtom of
-        stmnt2 ->                   %do
-            {NewI, Flag} = Result,
-            case Flag of
-                0 ->
-                    {fin, NewI, Flag};
-                1 ->
-                    sequence4opt(Steps, Source, Guard, Pid, NewI)
-            end;
-        stmnt10 ->                  %break
-            Act = Result,
-            {I, Act, 1};
-        stmnt81314 ->
-            Act = Result,
-            my_utility:addact(Act, Pid),
-            NewI = I + 1,
-            my_utility:genedge(Source, {Guard, Act}, NewI, Pid),
-            sequence4opt(Steps, NewI, true, Pid, NewI);
-        one_decl ->
-            Act = Result,
-            my_utility:addact(Act, Pid),
-            NewI = I + 1,
-            my_utility:genedge(Source, {Guard, Act}, NewI, Pid),
-            sequence4opt(Steps, NewI, true, Pid, NewI)
-    end.
 
 guard(GuardTree, Pid, I) ->
     [Stmnt4guard|GuardOpt] = GuardTree#tree.children,
