4b5936be63ba805fd21791f60f495b2e968dfe95
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2024-01-11 12:58:03 +0900

    gitignoreの追加

--- PROMELA2CS/gen.sh
+++ PROMELA2CS/gen.sh
@@ -1,6 +1,7 @@
 PARSER=/Users/yamakawamanobe/Master/promela_compiler/pmlparser2.py
 EDIT=/Users/yamakawamanobe/Master/promela_compiler/edit_result_analyze.py
 DIR=/Users/yamakawamanobe/Master/promela_compiler
+TESTDIR=/Users/yamakawamanobe/Master/promela_compiler/PROMELA2CS/testpml4gencsprime
 
 python3 $PARSER y $DIR/$1 | python3 $EDIT > hoge.txt
 echo "convartTree:start2(" `cat hoge.txt` ")."\
